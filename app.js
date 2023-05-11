9e7d6b2c10f556ad62b6c9bf4bedf9995fd7977a
Mike Eldredge <maeldredge.pro@gmail.com>
2023-05-11 10:30:43 -0600

    save school work

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const path = require('path');
 
+const port = 3000
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('users');
+});
+
+app.listen(port, () => {
+    console.log(`User Manager listening on port ${port}`);
+});
